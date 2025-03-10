b96ba63514a44f35d708a8aebdf72b88cb37a9cb
Arthur <nonakaodaarthur@gmail.com>
2025-03-10 13:52:42 -0300

    desig

--- script.js
+++ script.js
@@ -16,3 +16,49 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+particlesJS(
+  "particles-js",
+  {
+    particles: {
+      number: {
+        value: 50,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: ["#ffffff", "#000"],
+      },
+      shape: {
+        type: "circle",
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+      },
+      size: {
+        value: 3,
+        random: true,
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: {
+          value: ["#ffffff", "#000", "#2223aa"],
+        },
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+      },
+    },
+    retina_detect: true,
+  },
+  function () {
+    console.log("particles.js loaded - callback");
+  }
+);
